c741df468efaab2325191044e4182dad3ac8a6c0
Malik <huben92@gmail.com>
2026-05-15 13:36:30 +0700

    add port from env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +14,17 @@ func main() {
 	// Associate the root path "/" with our helloHandler function
 	http.HandleFunc("/", helloHandler)
 
+	// 1. Read the port provided dynamically by Vercel
+	port := os.Getenv("PORT")
+
+	// 2. Fallback to 3000 (or any port) for local development
+	if port == "" {
+		port = "3000"
+	}
+
 	// Start the server on port 5004
 	fmt.Println("Server is running on http://localhost:5004")
-	err := http.ListenAndServe(":5004", nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
